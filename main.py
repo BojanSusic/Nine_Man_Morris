48ed761312802a20a81556f37aba2558924f8d5a
BojanSusic <60573368+BojanSusic@users.noreply.github.com>
2021-04-14 19:30:53 +0200

    Fixed closing bug and add line drawing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import copy
-from turtle import goto
-
 import pygame, math, sys
 
 from alfabetarezanje import *
@@ -10,7 +8,7 @@ import time
 
 alfa = float('-inf')
 beta = float('inf')
-dubina = 4
+dubina = 3
 
 
 def trenutnaPloca(ploca):
@@ -43,15 +41,48 @@ boardCirclesProp = [[(370, 60), (0, 0, 0), 1], [(670, 60), (0, 0, 0), 1], [(970,
                     [(670, 160), (0, 0, 0), 1], [(870, 160), (0, 0, 0), 1], [(470, 360), (0, 0, 0), 1],
                     [(870, 360), (0, 0, 0), 1], [(470, 560), (0, 0, 0), 1], [(670, 560), (0, 0, 0), 1],
                     [(870, 560), (0, 0, 0), 1], [(570, 260), (0, 0, 0), 1], [(670, 260), (0, 0, 0), 1],
-                    [(770, 260), (0, 0, 0), 1], [(570, 360), (0, 0, 0), 1],
-                    [(770, 360), (0, 0, 0), 1], [(570, 460), (0, 0, 0), 1], [(670, 460), (0, 0, 0), 1],
-                    [(770, 460), (0, 0, 0), 1]]
+                    [(770, 260), (0, 0, 0), 1], [(570, 360), (0, 0, 0), 1], [(770, 360), (0, 0, 0), 1],
+                    [(570, 460), (0, 0, 0), 1], [(670, 460), (0, 0, 0), 1], [(770, 460), (0, 0, 0), 1]]
 circles = []
 
 
 def drawboard():
     circles.clear()
     screen.fill((255, 255, 255))
+    pygame.draw.line(screen, (0, 0, 0), (390, 60), (650, 60), 1)
+    pygame.draw.line(screen, (0, 0, 0), (690, 60), (950, 60), 1)
+    pygame.draw.line(screen, (0, 0, 0), (370, 80), (370, 340), 1)
+    pygame.draw.line(screen, (0, 0, 0), (370, 380), (370, 640), 1)
+    pygame.draw.line(screen, (0, 0, 0), (970, 80), (970, 340), 1)
+    pygame.draw.line(screen, (0, 0, 0), (970, 380), (970, 640), 1)
+    pygame.draw.line(screen, (0, 0, 0), (390, 660), (650, 660), 1)
+    pygame.draw.line(screen, (0, 0, 0), (690, 660), (950, 660), 1)
+
+    pygame.draw.line(screen, (0, 0, 0), (490, 160), (650, 160), 1)
+    pygame.draw.line(screen, (0, 0, 0), (690, 160), (850, 160), 1)
+    pygame.draw.line(screen, (0, 0, 0), (590, 260), (650, 260), 1)
+    pygame.draw.line(screen, (0, 0, 0), (690, 260), (750, 260), 1)
+    pygame.draw.line(screen, (0, 0, 0), (390, 360), (450, 360), 1)
+    pygame.draw.line(screen, (0, 0, 0), (490, 360), (550, 360), 1)
+    pygame.draw.line(screen, (0, 0, 0), (790, 360), (850, 360), 1)
+    pygame.draw.line(screen, (0, 0, 0), (890, 360), (950, 360), 1)
+    pygame.draw.line(screen, (0, 0, 0), (590, 460), (650, 460), 1)
+    pygame.draw.line(screen, (0, 0, 0), (690, 460), (750, 460), 1)
+    pygame.draw.line(screen, (0, 0, 0), (490, 560), (650, 560), 1)
+    pygame.draw.line(screen, (0, 0, 0), (690, 560), (850, 560), 1)
+    pygame.draw.line(screen, (0, 0, 0), (470, 180), (470, 340), 1)
+    pygame.draw.line(screen, (0, 0, 0), (470, 380), (470, 540), 1)
+    pygame.draw.line(screen, (0, 0, 0), (570, 280), (570, 340), 1)
+    pygame.draw.line(screen, (0, 0, 0), (570, 380), (570, 440), 1)
+    pygame.draw.line(screen, (0, 0, 0), (870, 180), (870, 340), 1)
+    pygame.draw.line(screen, (0, 0, 0), (870, 380), (870, 540), 1)
+    pygame.draw.line(screen, (0, 0, 0), (770, 280), (770, 340), 1)
+    pygame.draw.line(screen, (0, 0, 0), (770, 380), (770, 440), 1)
+
+    pygame.draw.line(screen, (0, 0, 0), (670, 80), (670, 140), 1)
+    pygame.draw.line(screen, (0, 0, 0), (670, 180), (670, 240), 1)
+    pygame.draw.line(screen, (0, 0, 0), (670, 480), (670, 540), 1)
+    pygame.draw.line(screen, (0, 0, 0), (670, 580), (670, 640), 1)
     for boardCircle in boardCirclesProp:
         circles.append(pygame.draw.circle(screen, boardCircle[1], boardCircle[0], 20, boardCircle[2]))
 
@@ -111,7 +142,7 @@ while running:
                                     pozicija = i
 
                         if event.type == pygame.QUIT:
-                            running = False
+                            quit()
                 # Ukoliko je mjesto prazno, onda ga mozemo popuniti
                 if board[pozicija] == "X":
                     board[pozicija] = "1"
@@ -136,7 +167,7 @@ while running:
                                                     boardCirclesProp[i][2] = 0
                                                     drawboard()
                                         if event.type == pygame.QUIT:
-                                            running = False
+                                            quit()
                                 # pozicija = int(input("\n Ukloni jedan protivnički '2' kamenčić: "))
 
                                 if board[pozicija] == "2" and not imaLiIkoMill(pozicija, board) or (
@@ -188,7 +219,7 @@ while running:
                                     pozicija = i
 
                         if event.type == pygame.QUIT:
-                            running = False
+                            quit()
 
                 while board[pozicija] != '1':
                     pozicija = -1
@@ -204,7 +235,7 @@ while running:
                                         pozicija = i
 
                             if event.type == pygame.QUIT:
-                                running = False
+                                quit()
 
                 igracZauzeoNovoMesto = False
                 while not igracZauzeoNovoMesto:
@@ -221,7 +252,7 @@ while running:
                                         nova_pozicija = i
 
                             if event.type == pygame.QUIT:
-                                running = False
+                                quit()
                     if board[nova_pozicija] == "X":
                         board[pozicija] = 'X'
                         board[nova_pozicija] = '1'
@@ -244,7 +275,7 @@ while running:
                                                         pozicija = i
 
                                             if event.type == pygame.QUIT:
-                                                running = False
+                                                quit()
 
                                     if board[pozicija] == "2" and not imaLiIkoMill(pozicija, board) or (
                                             imaLiIkoMill(pozicija, board) and brojKonkretnihPoteza(board, "1") == 3):
